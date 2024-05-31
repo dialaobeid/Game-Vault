064d106e1654d8eb688fbfccf5c04541ca42bd1b
dobeid <dialajo9@gmail.com>
2024-05-30 21:36:48 -0700

    updated header layout

--- client/src/components/Page/Header.jsx
+++ client/src/components/Page/Header.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { isAuthenticated, logout } from '../../utils/auth';
 
 const Header = () => {
+  // restricts access for only logged-in users
+  const auth = isAuthenticated();
+
   return (
     <header>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/library">Library</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/signup">Signup</Link></li>
+          {auth ? (
+            <>
+              <li><Link to="/profile">Profile</Link></li>
+              <li><Link to="/library">Library</Link></li>
+              <li><Link to="/" onClick={logout}>Logout</Link></li>
+            </>
+          ) : (
+            <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/signup">Signup</Link></li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
