0c4425e2bef83dbac28a778eed46d6b5bc08e271
dobeid <dialajo9@gmail.com>
2024-05-30 21:34:03 -0700

    updated profile layout

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Page/Header';
 import Footer from '../components/Page/Footer';
+import GameSearch from '../components/Game/GameSearch';
 
 const HomePage = () => {
   return (
@@ -8,7 +9,8 @@ const HomePage = () => {
       <Header />
       <main>
         <h1>Welcome to Player Archive</h1>
-        <p>Your ultimate gaming library</p>
+        <p>Your ultimate gaming companion.</p>
+        <GameSearch />
       </main>
       <Footer />
     </div>
