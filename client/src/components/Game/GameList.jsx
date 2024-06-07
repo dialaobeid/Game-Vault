10fa55819938537488fcc42d586c39fafdc78a3a
DannyM15 <merinodanny15@gmail.com>
2024-06-06 20:22:32 -0700

    images on search

--- client/src/components/Game/GameList.jsx
+++ client/src/components/Game/GameList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import GameCard from './GameCard';
+import { background } from '@chakra-ui/react';
 
 // Displays list of game cards
 const GameList = ({ games }) => {
   return (
     <div>
       {games.map(game => (
-        <GameCard key={game.id} game={game} />
+        <GameCard key={game.id} game={game}  />
       ))}
     </div>
   );
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,30 +1,3 @@
-//const url = 'https://rawg-video-games-database.p.rapidapi.com/games';
-//const options = {
-//	method: 'GET',
-//	headers: {
-//		'X-RapidAPI-Key': 'd1e0c52c1cmsh615ad3b8c909815p103232jsn3828439144da',
-//		'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
-//	}
-//};
-//
-//try {
-//	const response = await fetch(url, options);
-//	const result = await response.text();
-//	console.log(result);
-//} catch (error) {
-//	console.error(error);
-//}
-
-// export const searchVideoGames = (query) => {
-//  return fetch(`https://rawg-video-games-database.p.rapidapi.com/games${query}`);
-// };
-
-// export const searchVideoGames = (query) => {
-//   return fetch(`https://api.rawg.io/api/games?key=192b63954c724fb7b71b565cf2c30c0f${query}`);
-//  };
-
-
-
 import axios from 'axios';
 
 const API_KEY = import.meta.env.RAWG_API_KEY;
