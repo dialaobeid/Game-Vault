700451c3b4729d2c16f2d93b6e38a37c7f20be6d
dobeid <dialajo9@gmail.com>
2024-06-10 18:09:38 -0700

    progress bar display

--- client/src/components/Game/GameProgress.jsx
+++ client/src/components/Game/GameProgress.jsx
@@ -1,14 +1,4 @@
-// Displays user's game progress in %
-// const GameProgress = ({ progress }) => {
-//   return (
-//     <div>
-//       <h3>Game Progress: {progress}%</h3>
-//       <progress value={progress} max="100"></progress>
-//     </div>
-//   );
-
 import React from 'react';
-import { Box, Text, Progress } from '@chakra-ui/react';
 
 const GameProgress = ({ progress = 0 }) => {
   const getProgressColor = (progress) => {
@@ -18,16 +8,10 @@ const GameProgress = ({ progress = 0 }) => {
   };
 
   return (
-    <Box width="full">
-      <Text mb="2" color="white">Progress: {progress}%</Text>
-      <Progress
-        value={progress}
-        colorScheme={getProgressColor(progress)}
-        size="lg"
-        hasStripe
-        isAnimated
-      />
-    </Box>
+    <div className="progress-container">
+      <h6>Game Progress: {progress}%</h6>
+      <div className="progress-bar" style={{ width: `${progress}%`, backgroundColor: getProgressColor(progress) }}></div>
+    </div>
   );
 };
 
